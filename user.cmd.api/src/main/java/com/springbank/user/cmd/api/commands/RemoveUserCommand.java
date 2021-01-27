d5e96b31b7a2647e660396a2e03fb9c1450c4a7c
Temenuzhka Vuchova <tvuchova@gmail.com>
2021-01-27 08:53:41 +0200

    Initial commit

---
+++ user.cmd.api/src/main/java/com/springbank/user/cmd/api/commands/RemoveUserCommand.java
@@ -0,0 +1,13 @@
+package com.springbank.user.cmd.api.commands;
+
+import com.springbank.user.core.models.User;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+public class RemoveUserCommand {
+    @TargetAggregateIdentifier
+    private String id;
+
+
+}
